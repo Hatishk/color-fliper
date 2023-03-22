ba529f5434cbd75416bb3cf7312ec641a62c408d
Hatish <himanshuasija786@gmail.com>
2023-03-22 20:03:43 +0530

    javascript change color function added

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', function(){
+    // get random number b/w 0-3
+    const randomNumber = getRandomnumber();
+    document.body.style.backgroundColor =colors[randomNumber];
+    color.textContent =colors[randomNumber];
+});
+
+function getRandomnumber() {
+    return Math.floor(Math.random()* colors.length);
+    
+}
